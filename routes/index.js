825893efb56349fab159aea0b95c9878aca407a6
Ubuntu <imdesigner2012@gmail.com>
2017-03-24 04:18:34 +0000

    Style

--- routes/index.js
+++ routes/index.js
@@ -56,28 +56,44 @@ router.get('/get_data', function(req, res, next) {
 });
 
 
+router.get('/get_clients', function(req, res, next) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+
+  collection.find()
+  .then(function(clientRes){
+    console.log(clientRes);
+    res.send(clientRes);
+  });
+
+});
+
+
 router.get('/create_tote/:data', function(req, res, next) {
   var db = req.db;
   var collection = db.get('usercollection');
   var clientId = req.query.clientId;
   var toteName = req.query.toteName;
 
+  if (toteName !== '') {
 
-
-  collection.update(
-    {clientId: clientId},
-    { $push:
-      { totes:
-        { toteName: toteName, sensorId: '', readings: [] }
+    collection.update(
+      {clientId: clientId},
+      { $push:
+        { totes:
+          { toteName: toteName, sensorId: '', scaleId: '', sensorReadings: [], scaleReadings: [] }
+        }
       }
-    }
-  )
-  .then(function(result){
+    )
+    .then(function(result){
+      res.redirect('back');
+    })
+
+  } else {
     res.redirect('back');
-  })
+  }
 
 });
-
 
 router.get('/asign_sensor/:data', function(req, res, next) {
   var db = req.db;
@@ -86,9 +102,38 @@ router.get('/asign_sensor/:data', function(req, res, next) {
   var toteName = req.query.toteName;
   var sensorId = parseInt(req.query.sensorId);
 
+
+  collection.update(
+    { "totes.sensorId": sensorId },
+    { $set: {"totes.$.sensorId": ''} }
+  )
+
   collection.update(
     { clientId: clientId, "totes.toteName": toteName },
     { $set: {"totes.$.sensorId": sensorId} }
+  )
+  .then(function(result){
+    res.redirect('back');
+  })
+
+});
+
+router.get('/asign_scale/:data', function(req, res, next) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  var clientId = req.query.clientId;
+  var toteName = req.query.toteName;
+  var scaleId = parseInt(req.query.scaleId);
+
+
+  collection.update(
+    { "totes.scaleId": scaleId },
+    { $set: {"totes.$.scaleId": ''} }
+  )
+
+  collection.update(
+    { clientId: clientId, "totes.toteName": toteName },
+    { $set: {"totes.$.scaleId": scaleId} }
   )
   .then(function(result){
     res.redirect('back');
@@ -102,14 +147,29 @@ router.post("/post_data/:data", function(req, res){
   var collection = db.get('usercollection');
   var sensorRequest = req.params.data;
   var obj = JSON.parse(sensorRequest);
-  // obj.datetime = Date.now;
 
-  collection.update(
-    {"totes.sensorId": obj.sensorid},
-    { $push:
-      { "totes.$.readings": obj }
-    }
-  )
+  console.log('WEIGHT', obj);
+
+  if (obj.sensorid == 22) {
+
+    collection.update(
+      {"totes.scaleId": obj.sensorid},
+      { $push:
+        { "totes.$.scaleReadings": obj }
+      }
+    )
+
+  } else {
+
+    collection.update(
+      {"totes.sensorId": obj.sensorid},
+      { $push:
+        { "totes.$.sensorReadings": obj }
+      }
+    )
+
+  }
+
 
   return;
 
